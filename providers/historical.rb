6d1b4cda349e361b03e03126576deca70b26085a
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2016-06-29 15:18:27 +0200

    Add historical (provider and resource).

--- providers/historical.rb
+++ providers/historical.rb
@@ -2,22 +2,162 @@
 #
 # Provider:: historical
 #
+include Druid::Historical
+include Druid::Helper
 
 action :add do
   begin
-    # ... your code here ...
+    parent_config_dir = "/etc/druid"
+    config_dir = "#{parent_config_dir}/historical"
+    user = new_resource.user
+    group = new_resource.group
+    name = new_resource.name
+    cdomain = new_resource.cdomain
+    port = new_resource.port
+    parent_log_dir = new_resource.parent_log_dir
+    suffix_log_dir = new_resource.suffix_log_dir
+    log_dir = "#{parent_log_dir}/#{suffix_log_dir}"
+    segment_cache_dir = new_resource.segment_cache_dir
+    memcached_hosts = new_resource.memcached_hosts
+    processing_threads = new_resource.processing_threads
+    groupby_max_intermediate_rows = new_resource.groupby_max_intermediate_rows
+    groupby_max_results = new_resource.groupby_max_results
+    disk_size_kb = new_resource.disk_size_kb
+    tier = new_resource.tier
+    cpu_num = new_resource.cpu_num
+    memory_kb = new_resource.memory_kb
+
+    service "druid-historical" do
+      supports :status => true, :start => true, :restart => true, :reload => true
+      action :nothing
+     end
+
+    user user do
+      action :create
+    end
+
+    [ parent_config_dir, config_dir].each do |path|
+        directory path do
+         owner "root"
+         group "root"
+         mode 0700
+        end
+    end
+
+    [ parent_log_dir, log_dir ].each do |path|
+        directory path do
+          owner user
+          group group
+          mode 0700
+        end
+    end
+
+    directory segment_cache_dir do
+      owner user
+      group group
+      mode 0700
+      recursive true
+    end
+
+
+    #####################################
+    # Historical resource configuration #
+    #####################################
+
+    # Compute the number of processing threads based on CPUs
+    processing_threads = cpu_num > 1 ? cpu_num - 1 : 1 if processing_threads.nil?
+    heap_historical_memory_kb, processing_memory_buffer_b = compute_memory(memory_kb, processing_threads)
+    offheap_historical_memory_kb = (processing_memory_buffer_b * (processing_threads + 1) / 1024).to_i
+    free_memory_kb = heap_historical_memory_kb - offheap_historical_memory_kb - (1024*1024) # This is the overheap.
+    segments_memory_b = (free_memory_kb > 0 ? free_memory_kb : 0).to_i * 1024
+
+    max_size_b = 0
+
+    if tier == "hot"
+      max_size_b = segments_memory_b
+    else
+      max_size_b = segments_memory_b + disk_size_kb * 1024
+    end
+
+    Chef::Log.info(
+      "\nHistorical Memory:
+        * Memory: #{memory_kb}k 
+        * Heap: #{heap_historical_memory_kb}kb 
+        * ProcessingBuffer: #{processing_memory_buffer_b / 1024}kb
+        * OffHeap: #{offheap_historical_memory_kb}kb"
+    )
+
+    #####################################
+    #####################################
+
+    template "#{config_dir}/runtime.properties" do
+      source "historical.properties.erb"
+      owner "root"
+      group "root"
+      cookbook "druid"
+      mode 0644
+      retries 2
+      variables(:name => name, :cdomain => cdomain, :port => port, :memcached_hosts => memcached_hosts, 
+                :processing_threads => processing_threads, :processing_memory_buffer_b => processing_memory_buffer_b,
+                :groupby_max_intermediate_rows => groupby_max_intermediate_rows, :groupby_max_results => groupby_max_results,
+                :max_size_b => max_size_b, :tier => tier, :segment_cache_dir => segment_cache_dir)
+      notifies :restart, 'service[druid-historical]', :delayed
+    end
+
+    # service "druid-historical" do
+    #   supports :status => true, :start => true, :restart => true, :reload => true
+    #   action :start
+    # end
+
     Chef::Log.info("Druid Historical has been configurated correctly.")
   rescue => e
-    Chef::Log.error(e.message)
+    Chef::Log.error(e)
   end
 end
 
 action :remove do
   begin
-     # ... your code here ... 
+    parent_config_dir = "/etc/druid"
+    config_dir = "#{parent_config_dir}/historical"
+    parent_log_dir = new_resource.parent_log_dir
+    suffix_log_dir = new_resource.suffix_log_dir
+    log_dir = "#{parent_log_dir}/#{suffix_log_dir}"
+    segment_cache_dir = new_resource.segment_cache_dir
+
+    service "druid-historical" do
+      supports :status => true, :start => true, :restart => true, :reload => true
+      action :stop
+    end
+
+    dir_list = [
+      config_dir,
+      log_dir,
+      segment_cache_dir
+    ]  
+
+    template_list = [
+      "#{config_dir}/runtime.properties"
+    ]
+
+    template_list.each do |temp|
+       file temp do
+         action :delete
+       end
+    end
+
+    dir_list.each do |dir|
+       directory dir do
+         recursive true
+         action :delete
+       end
+    end
+
+    # Remove parent log directory if it doesn't have childs
+    delete_if_empty(parent_log_dir)
+
      Chef::Log.info("Druid Historical has been deleted correctly.")
   rescue => e
-    Chef::Log.error(e.message)
+    Chef::Log.error(e)
   end
 end
 
--- resources/historical.rb
+++ resources/historical.rb
@@ -6,5 +6,20 @@
 actions :add, :remove
 default_action :add
 
-attribute :memory, :kind_of => String, :default => "512"
+attribute :user, :kind_of => String, :default => "druid"
+attribute :group, :kind_of => String, :default => "druid"
+attribute :name, :kind_of => String, :default => "localhost"
+attribute :cdomain, :kind_of => String, :default => "redborder.cluster"
+attribute :port, :kind_of => Integer, :default => 8091
+attribute :parent_log_dir, :kind_of => String, :default => "/var/log/druid"
+attribute :suffix_log_dir, :kind_of => String, :default => "historical"
+attribute :segment_cache_dir, :kind_of => String, :default => "/var/druid/historical/indexCache"
+attribute :memcached_hosts, :kind_of => String
+attribute :processing_threads, :kind_of => Integer
+attribute :groupby_max_intermediate_rows, :kind_of => Integer, :default => 50000
+attribute :groupby_max_results, :kind_of => Integer, :default => 500000
+attribute :tier, :kind_of => String, :default => "default"
+attribute :cpu_num, :kind_of => Integer, :default => 4
+attribute :memory_kb, :kind_of => Integer, :default => 8388608
+attribute :disk_size_kb, :kind_of => Integer, :default => 10485760
 
