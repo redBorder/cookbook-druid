b502a181c324ca3ca6eaed7628df99b58a9cdf25
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2016-06-29 14:59:14 +0200

    Create Druid::Helper module.

--- libraries/directory.rb
+++
@@ -1,9 +0,0 @@
-def delete_if_empty(directory)
-   if Dir["#{directory}/*"].empty? 
-      directory directory do
-        action :delete
-      end
-   end
-end
- 
-
---
+++ libraries/helper.rb
@@ -0,0 +1,11 @@
+module Druid
+    module Helper
+       def delete_if_empty(directory)
+   	   	    if Dir["#{directory}/*"].empty? 
+        		 directory directory do
+         		   action :delete
+        	     end
+  	        end
+  	    end
+    end
+end
