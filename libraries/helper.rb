5db6bc9aeb9808f9cd49dd74833ea64eeddb56bf
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2016-07-04 09:24:10 +0200

    Add all_services_disable? function.

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -1,11 +1,19 @@
 module Druid
    module Helper
+
+      # Delete a directory if it is empty.
       def delete_if_empty(directory)
          if Dir["#{directory}/*"].empty? 
             directory directory do
             action :delete
             end
          end
-       end
+      end
+
+      # Check if all druid services are disabled.
+      def all_services_disable?
+      	node["druid"]["services"].map{|service,status| status}.all? {|status| !status }
+      end
+
     end
 end
