a4aefab23f3c4076d2dfe9b1fd322928efa88c09
Alberto Rodríguez <arodriguezdlc@gmail.com>
2017-01-19 18:58:24 +0100

    Fix num cpu default value for middlemanager

--- resources/resources/middlemanager.rb
+++ resources/resources/middlemanager.rb
@@ -23,7 +23,7 @@ attribute :indexing_dir, :kind_of => String, :default => "/tmp/druid/indexing"
 attribute :hadoop_version, :kind_of => String, :default => "2.7.1"
 attribute :rmi_address, :kind_of => String, :default => "127.0.0.1"
 attribute :capacity_multiplier, :kind_of => Integer, :default => 1
-attribute :cpu_num, :kind_of => Integer, :default => 1
+attribute :cpu_num, :kind_of => Integer, :default => node["cpu"]["total"]
 attribute :memory_kb, :kind_of => Integer, :default => 3145728
 attribute :worker_capacity, :kind_of => Integer
 attribute :processing_memory_buffer_b, :kind_of => Integer
