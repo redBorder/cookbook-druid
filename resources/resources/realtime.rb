acff5b24d6a260c1192646f2aba38c19a2c1a28a
Juan J. Chorro <jjchorrobe@gmail.com>
2018-01-26 13:28:21 +0100

    Delete S3 references

--- resources/resources/realtime.rb
+++ resources/resources/realtime.rb
@@ -13,10 +13,6 @@ attribute :cdomain, :kind_of => String, :default => "redborder.cluster"
 attribute :parent_log_dir, :kind_of => String, :default => "/var/log/druid"
 attribute :suffix_log_dir, :kind_of => String, :default => "realtime"
 attribute :port, :kind_of => Integer, :default => 8084
-attribute :s3_access_key, :kind_of => String
-attribute :s3_secret_key, :kind_of => String
-attribute :s3_log_bucket, :kind_of => String
-attribute :s3_log_prefix, :kind_of => String, :default => "druid-indexer-logs"
 attribute :base_dir, :kind_of => String, :default => "/tmp/druid"
 attribute :cpu_num, :kind_of => Integer, :default => node["cpu"]["total"]
 attribute :memory_kb, :kind_of => Integer, :default => 3145728
