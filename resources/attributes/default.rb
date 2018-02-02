9012bce1c0230c0d716ee6ea53592da9af92d4d3
Juan J. Chorro Bergillo <jjchorrobe@gmail.com>
2018-02-02 08:32:49 +0100

    Update cookbook to add realtime mode (#11)
    * Feature/10602/Task/10626: Update cookbook to add realtime mode

--- resources/attributes/default.rb
+++ resources/attributes/default.rb
@@ -4,3 +4,4 @@ default["druid"]["coordinator"]["registered"] = false
 default["druid"]["historical"]["registered"] = false
 default["druid"]["middlemanager"]["registered"] = false
 default["druid"]["overlord"]["registered"] = false
+default["druid"]["realtime"]["registered"] = false
