85549ec56d85358e12f32b8d7ea33cd87c0ffe33
Miguel Negrón <45871721+manegron@users.noreply.github.com>
2024-05-26 09:50:44 +0100

    Update metadata.rb

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -1,7 +1,6 @@
 name             'druid'
-maintainer       'redborder'
-maintainer_email 'manegron@redborder.com, dvanhoucke@redborder.com'
-license          'All rights reserved'
+maintainer       'Eneo Tecnología S.L.'
+maintainer_email 'git@redborder.com'
+license          'AGPL-3.0'
 description      'Installs/Configures druid'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.4.10'
