7f60960cc9ac8174d540fe1e374ab4051bb50912
Miguel Negron <manegron@redborder.com>
2024-06-01 20:16:55 +0100

    lint attributes

--- resources/attributes/default.rb
+++ resources/attributes/default.rb
@@ -1,7 +1,7 @@
-#Flags
-default["druid"]["broker"]["registered"] = false
-default["druid"]["coordinator"]["registered"] = false
-default["druid"]["historical"]["registered"] = false
-default["druid"]["middlemanager"]["registered"] = false
-default["druid"]["overlord"]["registered"] = false
-default["druid"]["realtime"]["registered"] = false
+# Flags
+default['druid']['broker']['registered'] = false
+default['druid']['coordinator']['registered'] = false
+default['druid']['historical']['registered'] = false
+default['druid']['middlemanager']['registered'] = false
+default['druid']['overlord']['registered'] = false
+default['druid']['realtime']['registered'] = false
