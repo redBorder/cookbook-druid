99f871c588b5489c19b6c3f8fb8936a2d1948a9f
Miguel Negron <manegron@redborder.com>
2024-06-01 20:17:11 +0100

    lint recipes

--- resources/recipes/default.rb
+++ resources/recipes/default.rb
@@ -1,47 +1,42 @@
-#
-# Cookbook Name:: druid
+# Cookbook:: druid
 # Recipe:: default
-#
-# Copyright 2016, redborder
-#
-# AFFERO GENERAL PUBLIC LICENSE V3
-#
+# Copyright:: 2024, redborder
+# License:: Affero General Public License, Version 3
 
 memory_mb = (node['memory']['total'].match(/\A(?<value>\d+)(?<modifier>\w+)\z/)[:value].to_i / 1024 * 0.90).to_i
 
-druid_historical "Configure Druid Historical" do
-  name "localhost"
+druid_historical 'Configure Druid Historical' do
+  name 'localhost'
   memory_kb (memory_mb * 0.2).to_i
   action :add
 end
 
-druid_broker "Configure Druid Broker" do
-  name "localhost"
+druid_broker 'Configure Druid Broker' do
+  name 'localhost'
   memory_kb (memory_mb * 0.2).to_i
   action :add
 end
 
-druid_coordinator "Configure Druid coordinator" do
-  name "localhost"
+druid_coordinator 'Configure Druid coordinator' do
+  name 'localhost'
   memory_kb (memory_mb * 0.1).to_i
   action :add
 end
 
-druid_overlord "Configure Druid overlord" do
-  name "localhost"
+druid_overlord 'Configure Druid overlord' do
+  name 'localhost'
   memory_kb (memory_mb * 0.1).to_i
   action :add
 end
 
-druid_middlemanager "Configure Druid middlemanager" do
-  name "localhost"
+druid_middlemanager 'Configure Druid middlemanager' do
+  name 'localhost'
   memory_kb (memory_mb * 0.4).to_i
   action :add
 end
 
-druid_realtime "Configure Druid realtime" do
-  name "localhost"
+druid_realtime 'Configure Druid realtime' do
+  name 'localhost'
   memory_kb (memory_mb * 0.4).to_i
   action :add
 end
-
