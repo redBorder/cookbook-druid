f93ae7917ff26500c1a9edb5c3b00abc01e3a1f6
jnavarrorb <jnavarro@redborder.com>
2024-08-05 09:46:13 +0100

    undo cookbookstyle changes

--- resources/recipes/default.rb
+++ resources/recipes/default.rb
@@ -6,37 +6,37 @@
 memory_mb = (node['memory']['total'].match(/\A(?<value>\d+)(?<modifier>\w+)\z/)[:value].to_i / 1024 * 0.90).to_i
 
 druid_historical 'Configure Druid Historical' do
-  druid_historical_name 'localhost'
+  name 'localhost'
   memory_kb (memory_mb * 0.2).to_i
   action :add
 end
 
 druid_broker 'Configure Druid Broker' do
-  druid_broker_name 'localhost'
+  name 'localhost'
   memory_kb (memory_mb * 0.2).to_i
   action :add
 end
 
 druid_coordinator 'Configure Druid coordinator' do
-  druid_coordinator_name 'localhost'
+  name 'localhost'
   memory_kb (memory_mb * 0.1).to_i
   action :add
 end
 
 druid_overlord 'Configure Druid overlord' do
-  druid_overlord_name 'localhost'
+  name 'localhost'
   memory_kb (memory_mb * 0.1).to_i
   action :add
 end
 
 druid_middlemanager 'Configure Druid middlemanager' do
-  druid_middlemanager_name 'localhost'
+  name 'localhost'
   memory_kb (memory_mb * 0.4).to_i
   action :add
 end
 
 druid_realtime 'Configure Druid realtime' do
-  druid_realtime_name 'localhost'
+  name 'localhost'
   memory_kb (memory_mb * 0.4).to_i
   action :add
 end
--- resources/resources/broker.rb
+++ resources/resources/broker.rb
@@ -6,7 +6,7 @@ default_action :add
 
 attribute :user, kind_of: String, default: 'druid'
 attribute :group, kind_of: String, default: 'druid'
-attribute :druid_broker_name, kind_of: String, default: 'localhost'
+attribute :name, kind_of: String, default: 'localhost'
 attribute :cdomain, kind_of: String, default: 'redborder.cluster'
 attribute :port, kind_of: Integer, default: 8080
 attribute :parent_log_dir, kind_of: String, default: '/var/log/druid'
