344900256b73f78c3282076239ffad96c6c0713e
nilsver <139124949+nilsver@users.noreply.github.com>
2025-07-08 08:41:41 +0100

    Release 3.1.4 (#84)
    * Improvement/#19203 Refactor S3 Variables Passing in Cookbooks (#83)
    
    * Refactor S3 configuration handling to use s3_secrets attribute instead of individual keys
    
    * Add s3_secrets attribute to indexer resource and update configuration handling
    
    ---------
    
    Co-authored-by: Pablo Pérez <pperez@redborder.com>
    
    * Release 3.1.4
    
    ---------
    
    Co-authored-by: Rafa Gómez <rgomez@redborder.com>
    Co-authored-by: Pablo Pérez <pperez@redborder.com>

--- resources/resources/common.rb
+++ resources/resources/common.rb
@@ -12,11 +12,9 @@ attribute :memcached_hosts, kind_of: Array, default: ['memcached.service:11211']
 attribute :psql_uri, kind_of: String
 attribute :psql_user, kind_of: String
 attribute :psql_password, kind_of: String
-attribute :s3_bucket, kind_of: String
-attribute :s3_access_key, kind_of: String
-attribute :s3_secret_key, kind_of: String
 attribute :s3_region, kind_of: String, default: 'us-east-1'
 attribute :s3_prefix, kind_of: String, default: 'rbdata'
 attribute :s3_service, kind_of: String, default: 's3.service'
 attribute :s3_port, kind_of: Integer, default: 9000
 attribute :druid_local_storage_dir, kind_of: String, default: '/tmp/druid/localStorage'
+attribute :s3_secrets, kind_of: Hash, default: {}
